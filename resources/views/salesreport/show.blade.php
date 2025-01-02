9876c2ba58e0774a5a2fcd9d1bbd521f263c43aa
Dafis Wahyuni <dafis23ti@mahasiswa.pcr.ac.id>
2025-01-02 11:33:15 +0700

    perubahan lagi

--- resources/views/salesreport/show.blade.php
+++ resources/views/salesreport/show.blade.php
@@ -1,19 +1,47 @@
+{{-- resources/views/salesreport/show.blade.php --}}
 @extends('layout.sales')
 
-@section('title', 'Menu Details')
-@yield
+@section('title', 'Sales Report Details')
+
 @section('content')
-    <h1>Menu Details</h1>
-
-    <div class="mb-3">
-        <strong>Name:</strong> {{ $menu->name }}
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <h3>Sales Report Details</h3>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <tr>
+                    <th>Name</th>
+                    <td>{{ $salesreport->name }}</td>
+                </tr>
+                <tr>
+                    <th>Description</th>
+                    <td>{{ $salesreport->description ?? 'No description available' }}</td>
+                </tr>
+                <tr>
+                    <th>Price</th>
+                    <td>${{ number_format($salesreport->price, 2) }}</td>
+                </tr>
+                <tr>
+                    <th>Created At</th>
+                    <td>{{ $salesreport->created_at->format('d-m-Y H:i') }}</td>
+                </tr>
+                <tr>
+                    <th>Updated At</th>
+                    <td>{{ $salesreport->updated_at->format('d-m-Y H:i') }}</td>
+                </tr>
+            </table>
+        </div>
+        <div class="card-footer">
+            <a href="{{ route('salesreport.index') }}" class="btn btn-secondary">Back to List</a>
+            <a href="{{ route('salesreport.edit', $salesreport->id) }}" class="btn btn-warning">Edit</a>
+            <form action="{{ route('salesreport.destroy', $salesreport->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Are you sure you want to delete this item?');">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </div>
     </div>
-    <div class="mb-3">
-        <strong>Tabel number:</strong> {{ $menu->description ?? 'N/A' }}
-    </div>
-    <div class="mb-3">
-        <strong>Price:</strong> ${{ number_format($menu->price, 2) }}
-    </div>
-
-    <a href="{{ route('menu.index') }}" class="btn btn-secondary">Back to List</a>
+</div>
 @endsection
